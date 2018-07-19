fabebde253b3eb4a8ce23bfaa0494d008caa4d30
azaian <abdalmoniem.s.zaian@gmail.com>
2018-07-19 13:13:57 +0200

    init project

---
+++ main_gate0/main_gate0/library.c
@@ -0,0 +1,16 @@
+/*
+ * main_gate0.c
+ *
+ * Created: 7/19/2018 1:12:44 PM
+ * Author : zaian
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
